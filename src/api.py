d5d833ff2a3b1e41297524ac315bdcc8646c75ff
klapen <oasaavedrac@gmail.com>
2019-06-12 18:27:21 -0500

    Fix configuration to compile

--- src/api.py
+++ src/api.py
@@ -1,15 +1,26 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
+from config import db, app
+from models import (
+    Log,
+    LogSchema,
+)
+
 api = Api(app)
+
+class HistoryLog(Resource):
+    def get(self):
+        history = Log.query.order_by(Log.timestamp).all()
+        history_schema = LogSchema(many=True)
+        return history_schema.dump(history).data
 
 class ScreenshotImporter(Resource):
     def get(self):
         return { 'status': 'ok', 'version': 'v0.1' }
 
-api.add_resource(ScreenshotImporter,
-                 '/')
+api.add_resource(ScreenshotImporter, '/')
+api.add_resource(HistoryLog, '/log')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/config.py
+++ src/config.py
@@ -3,13 +3,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Configure the SQLAlchemy
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'main.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
